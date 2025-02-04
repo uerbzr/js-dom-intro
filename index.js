0cf32ab7ffdf637e18564d805ad559296de0a8c6
uerbzr <nigel.sibbert@gmail.com>
2025-02-04 08:32:40 +0000

    added click

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Hello, world!");
+const container = document.querySelector(".container");
+
+//const cellValues = ['O','X','X','X','O','X','O','X','O']
+//const cellValues = [null, null, null, null, null, null, null, null, null]
+const cellValues = Array(9).fill(null);
+let lastValue = ["X", "O"];
+
+function drawBoard() {
+  container.innerHTML = "";
+
+  for (let x = 0; x < 9; x++) {
+    //create cell
+    const cell = document.createElement("div");
+    //add css class for cell
+    cell.classList.add("cell");
+    console.log(cell);
+
+    //set symbol to contents of array
+    const symbol = document.createElement("p");
+    symbol.classList.add("symbol");
+    symbol.innerText = cellValues[x];
+
+    cell.addEventListener("click", function () {
+      lastValue.reverse();
+      cellValues[x] = lastValue[0];
+      drawBoard();
+      console.log(x);
+      console.log(cellValues);
+    });
+
+    container.append(cell);
+    cell.append(symbol);
+  }
+}
+
+drawBoard();
