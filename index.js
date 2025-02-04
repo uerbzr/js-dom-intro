1604027e75c790ce8cd67f831f0ff3147ec47917
uerbzr <nigel.sibbert@gmail.com>
2025-02-04 08:01:11 +0000

    simple task list

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello, world!");
+const tasks = [
+  {
+    id: 1,
+    title: "make bed",
+    isCompleted: true,
+  },
+  {
+    id: 2,
+    title: "make breakfast",
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    title: "make coffee",
+    isCompleted: true,
+  },
+];
+
+tasks.map((task) => {
+  console.log(task.title);
+  let el = document.createElement("li");
+  el.className = task.isCompleted ? "completed" : "notcompleted";
+  el.innerHTML = task.title;
+  document.getElementById("todo-list").appendChild(el);
+});
